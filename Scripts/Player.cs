dac9bca41aeadae1bee3869f22ec346bc7babbe7
DavidTorresOvallos <davidtorresovallos@gmail.com>
2023-08-15 19:49:54 -0500

    Player and World V1

---
+++ Scripts/Player.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+
+public class Player : KinematicBody2D
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+    public override void _Process(float delta)
+    {
+        Vector2 velocity = new Vector2();
+        if (Input.IsActionPressed("ui_left"))
+        {
+            velocity.x -= 50;
+        }
+        if (Input.IsActionPressed("ui_right"))
+        {
+            velocity.x += 50;
+        }
+        MoveAndSlide(velocity, Vector2.Up);
+    }
+}
